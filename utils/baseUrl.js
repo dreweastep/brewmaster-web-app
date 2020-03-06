581d0ea14e1d603bbfa25be546d29855ffcf6d14
Eastep <easandb@dunwoody.edu>
2020-03-06 14:50:46 -0600

    try self.location

--- utils/baseUrl.js
+++ utils/baseUrl.js
@@ -1,6 +1,6 @@
 const baseUrl =
     process.env.NODE_ENV === "production" 
-        ? `http://${location.hostname}:3000`
-        : `http://${location.hostname}:3000`
+        ? `http://${self.location.hostname}:3000`
+        : `http://${self.location.hostname}:3000`
 
 export default baseUrl;
